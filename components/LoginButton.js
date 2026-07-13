07fc07b4ef3952924fe0064f1da85729698c967e
Hrishikesh <157392774+HrishiSwant@users.noreply.github.com>
2026-07-13 20:08:09 +0530

    Add LoginButton component for Spotify sign-in

--- components/LoginButton.js
+++ components/LoginButton.js
@@ -1,1 +1,8 @@
+'use client';
+import { signIn } from 'next-auth/react';
 
+export default function LoginButton() {
+  return (
+    <button
+      onClick={() => signIn('spotify')}
+      className="bg-spotify-green hover:sc
