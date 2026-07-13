d5382a210a20403529d5f02a8fb3582b935e4cd0
Hrishikesh <157392774+HrishiSwant@users.noreply.github.com>
2026-07-13 20:06:29 +0530

    Add Providers component for session and player context

--- components/Providers.js
+++ components/Providers.js
@@ -1,1 +1,11 @@
+'use client';
+import { SessionProvider } from 'next-auth/react';
+import { PlayerProvider } from '@/context/PlayerContext';
 
+export default function Providers({ children }) {
+  return (
+    <SessionProvider>
+      <PlayerProvider>{children}</PlayerProvider>
+    </SessionProvider>
+  );
+}
