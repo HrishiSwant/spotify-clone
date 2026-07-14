172ef0fe7faf3e6fcddc4cfdf4c7a95dcb8371f6
Hrishikesh <157392774+HrishiSwant@users.noreply.github.com>
2026-07-14 21:55:19 +0530

    Enhance LoginButton styling and hover effect

--- components/LoginButton.js
+++ components/LoginButton.js
@@ -5,4 +5,9 @@ export default function LoginButton() {
   return (
     <button
       onClick={() => signIn('spotify')}
-      className="bg-spotify-green hover:sc
+      className="bg-spotify-green hover:scale-105 transition text-black font-bold rounded-full px-6 py-2"
+    >
+      Log in
+    </button>
+  );
+}
