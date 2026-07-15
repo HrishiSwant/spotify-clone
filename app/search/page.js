f22b5e458086e29ff76abf43d735c5b94195cfe1
Hrishikesh <157392774+HrishiSwant@users.noreply.github.com>
2026-07-15 20:06:31 +0530

    Add dynamic export to search page

--- app/search/page.js
+++ app/search/page.js
@@ -1,4 +1,5 @@
 'use client';
+export const dynamic = 'force-dynamic';
 import { useState, useEffect } from 'react';
 import SearchBar from '@/components/SearchBar';
 import TrackList from '@/components/TrackList';
