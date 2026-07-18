89fab2c6b1ee2a8ec5dbe99f31ecf316a3369d1e
Hrishikesh <157392774+HrishiSwant@users.noreply.github.com>
2026-07-18 19:55:37 +0530

    Add callback URL to Spotify sign-in

--- components/LoginButton.js
+++ components/LoginButton.js
@@ -1,10 +1,15 @@
 'use client';
+
 import { signIn } from 'next-auth/react';
 
 export default function LoginButton() {
   return (
     <button
-      onClick={() => signIn('spotify')}
+      onClick={() =>
+        signIn('spotify', {
+          callbackUrl: '/',
+        })
+      }
       className="bg-spotify-green hover:scale-105 transition text-black font-bold rounded-full px-6 py-2"
     >
       Log in
