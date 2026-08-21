d6262c9e4194523d9a7b8f7447bfd10704688da6
Hrishikesh <157392774+HrishiSwant@users.noreply.github.com>
2026-08-22 02:26:09 +0530

    Fix string quotes and update config matcher

--- middleware.js
+++ middleware.js
@@ -1,14 +1,14 @@
-export { default } from 'next-auth/middleware';
+export { default } from "next-auth/middleware";
 
 export const config = {
   matcher: [
-    '/',
-    '/search/:path*',
-    '/library/:path*',
-    '/playlist/:path*',
-    '/album/:path*',
-    '/artist/:path*',
-    '/liked/:path*',
-    '/profile/:path*',
+    "/",
+    "/search/:path*",
+    "/library/:path*",
+    "/playlist/:path*",
+    "/album/:path*",
+    "/artist/:path*",
+    "/liked/:path*",
+    "/profile/:path*",
   ],
 };
