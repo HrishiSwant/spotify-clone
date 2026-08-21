35f46a8c0368d1a37cf30fb9879c0f8ccfd45020
Hrishikesh <157392774+HrishiSwant@users.noreply.github.com>
2026-08-22 01:41:30 +0530

    Add Layout component using MainLayout

---
+++ app/(main)/layout.js
@@ -0,0 +1,11 @@
+'use client';
+
+import MainLayout from '@/components/layout/MainLayout';
+
+export default function Layout({ children }) {
+  return (
+    <MainLayout>
+      {children}
+    </MainLayout>
+  );
+}
