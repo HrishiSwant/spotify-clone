7b82a795565ee63f3c38bef876d31a39c5b75596
Hrishikesh <157392774+HrishiSwant@users.noreply.github.com>
2026-08-22 02:09:50 +0530

    Refactor QueueService to include shuffle and repeat

--- services/queueService.js
+++ services/queueService.js
@@ -1,12 +1,97 @@
 class QueueService {
   constructor() {
     this.queue = [];
+    this.history = [];
     this.currentIndex = -1;
+
+    this.shuffle = false;
+    this.repeat = false;
   }
 
-  load(tracks = [], startIndex = 0) {
-    this.queue = tracks;
-    this.currentIndex = startIndex;
+  setQueue(queue = []) {
+    this.queue = [...queue];
+    this.currentIndex = 0;
+  }
+
+  getQueue() {
+    return this.queue;
+  }
+
+  setCurrentTrack(track) {
+    const index = this.queue.findIndex(
+      (t) => t.id === track.id
+    );
+
+    if (index !== -1) {
+      this.currentIndex = index;
+    }
+  }
+
+  getCurrentTrack() {
+    return this.queue[this.currentIndex] || null;
+  }
+
+  next() {
+    if (!this.queue.length) return null;
+
+    if (this.shuffle) {
+      this.currentIndex = Math.floor(
+        Math.random() * this.queue.length
+      );
+
+      return this.getCurrentTrack();
+    }
+
+    if (
+      this.currentIndex <
+      this.queue.length - 1
+    ) {
+      this.currentIndex++;
+      return this.getCurrentTrack();
+    }
+
+    if (this.repeat) {
+      this.currentIndex = 0;
+      return this.getCurrentTrack();
+    }
+
+    return null;
+  }
+
+  previous() {
+    if (!this.queue.length) return null;
+
+    if (this.currentIndex > 0) {
+      this.currentIndex--;
+    }
+
+    return this.getCurrentTrack();
+  }
+
+  add(track) {
+    this.queue.push(track);
+  }
+
+  addNext(track) {
+    this.queue.splice(
+      this.currentIndex + 1,
+      0,
+      track
+    );
+  }
+
+  remove(trackId) {
+    this.queue = this.queue.filter(
+      (t) => t.id !== trackId
+    );
+
+    if (
+      this.currentIndex >=
+      this.queue.length
+    ) {
+      this.currentIndex =
+        this.queue.length - 1;
+    }
   }
 
   clear() {
@@ -14,81 +99,22 @@ class QueueService {
     this.currentIndex = -1;
   }
 
-  add(track) {
-    this.queue.push(track);
+  toggleShuffle() {
+    this.shuffle = !this.shuffle;
+    return this.shuffle;
   }
 
-  addMany(tracks = []) {
-    this.queue.push(...tracks);
+  toggleRepeat() {
+    this.repeat = !this.repeat;
+    return this.repeat;
   }
 
-  remove(trackId) {
-    this.queue = this.queue.filter(
-      (track) => track.id !== trackId
-    );
-
-    if (this.currentIndex >= this.queue.length) {
-      this.currentIndex = this.queue.length - 1;
-    }
+  isShuffleEnabled() {
+    return this.shuffle;
   }
 
-  next() {
-    if (this.currentIndex + 1 >= this.queue.length) {
-      return null;
-    }
-
-    this.currentIndex++;
-
-    return this.queue[this.currentIndex];
-  }
-
-  previous() {
-    if (this.currentIndex <= 0) {
-      return null;
-    }
-
-    this.currentIndex--;
-
-    return this.queue[this.currentIndex];
-  }
-
-  current() {
-    return this.queue[this.currentIndex] || null;
-  }
-
-  getQueue() {
-    return this.queue;
-  }
-
-  getIndex() {
-    return this.currentIndex;
-  }
-
-  setIndex(index) {
-    if (
-      index >= 0 &&
-      index < this.queue.length
-    ) {
-      this.currentIndex = index;
-    }
-
-    return this.current();
-  }
-
-  hasNext() {
-    return this.currentIndex < this.queue.length - 1;
-  }
-
-  hasPrevious() {
-    return this.currentIndex > 0;
-  }
-
-  size() {
-    return this.queue.length;
-  }
-
-  isEmpty() {
-    return this.queue.length === 0;
+  isRepeatEnabled() {
+    return this.repeat;
   }
 }
 
