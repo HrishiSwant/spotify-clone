96c5221d68f75dbfb63b92206427db99b58e211b
Hrishikesh <157392774+HrishiSwant@users.noreply.github.com>
2026-08-22 01:58:30 +0530

    Refactor layout with context providers and metadata
    Refactor layout to include multiple context providers and update metadata.

--- app/(main)/layout.js
+++ app/(main)/layout.js
@@ -1,11 +1,41 @@
-'use client';
+import './globals.css';
 
-import MainLayout from '@/components/layout/MainLayout';
+import { Inter } from 'next/font/google';
 
-export default function Layout({ children }) {
+import { SessionProvider } from 'next-auth/react';
+
+import { AuthProvider } from '@/context/AuthContext';
+import { PlayerProvider } from '@/context/PlayerContext';
+import { QueueProvider } from '@/context/QueueContext';
+import { ThemeProvider } from '@/context/ThemeContext';
+
+const inter = Inter({
+  subsets: ['latin'],
+});
+
+export const metadata = {
+  title: 'Spotify Clone',
+  description: 'Spotify Clone',
+};
+
+export default function RootLayout({
+  children,
+}) {
   return (
-    <MainLayout>
-      {children}
-    </MainLayout>
+    <html lang="en">
+      <body className={inter.className}>
+        <SessionProvider>
+          <ThemeProvider>
+            <AuthProvider>
+              <QueueProvider>
+                <PlayerProvider>
+                  {children}
+                </PlayerProvider>
+              </QueueProvider>
+            </AuthProvider>
+          </ThemeProvider>
+        </SessionProvider>
+      </body>
+    </html>
   );
 }
