5e7e975f22ef5a7e6f1f73d713ba4a797b820a9c
Hrishikesh <157392774+HrishiSwant@users.noreply.github.com>
2026-08-24 20:37:31 +0530

    Rename SpotifySDK component to SpotifyPlayer

--- components/player/SpotifySDK.js
+++ components/player/SpotifySDK.js
@@ -6,7 +6,7 @@ import { useSession } from 'next-auth/react';
 import { usePlayer } from '@/context/PlayerContext';
 import spotifyPlayer from '@/lib/spotify/player';
 
-export default function SpotifySDK() {
+export default function SpotifyPlayer() {
   const { data: session } = useSession();
 
   const {
