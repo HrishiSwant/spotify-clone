476c0746be0641aef55fcd8f89638ff1e66a30a2
Hrishikesh <157392774+HrishiSwant@users.noreply.github.com>
2026-08-31 19:39:41 +0530

    Include Spotify player script in layout
    Added Spotify player script to RootLayout.

--- app/layout.js
+++ app/layout.js
@@ -1,5 +1,6 @@
 import './globals.css';
 
+import Script from 'next/script';
 import { Inter } from 'next/font/google';
 
 import Providers from '@/components/Providers';
@@ -17,9 +18,16 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body className={inter.className}>
+
+        <Script
+          src="https://sdk.scdn.co/spotify-player.js"
+          strategy="afterInteractive"
+        />
+
         <Providers>
           {children}
         </Providers>
+
       </body>
     </html>
   );
